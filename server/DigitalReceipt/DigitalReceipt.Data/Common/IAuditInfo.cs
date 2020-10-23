c2b7e34376134122f16355b243405d5eb62dc30f
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-10-23 23:28:35 +0300

    Set up database layer

---
+++ server/DigitalReceipt/DigitalReceipt.Data/Common/IAuditInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DigitalReceipt.Data.Common
+{
+    public interface IAuditInfo
+    {
+        DateTime CreatedOn { get; set; }
+
+        DateTime? ModifiedOn { get; set; }
+    }
+}
