056c32781a59651857695206ea7607871ad7aca4
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-10-24 12:02:46 +0300

    Receipt services

---
+++ server/DigitalReceipt/DigitalReceipt.Models/Products/ProductInputModel.cs
@@ -0,0 +1,20 @@
+﻿using DigitalReceipt.Common.Mappings;
+using DigitalReceipt.Data.Models;
+using System.ComponentModel.DataAnnotations;
+using static DigitalReceipt.Common.ModelConstants.ProductConstants;
+
+namespace DigitalReceipt.Models.Products
+{
+    public class ProductInputModel : IMapTo<Product>
+    {
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(BarcodeMaxLength)]
+        public string Barcode { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
