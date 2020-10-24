499bff3efc8a59d0870734d1c6000be40c636358
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-10-24 12:21:47 +0300

    Seeders

--- server/DigitalReceipt/DigitalReceipt.Common/GlobalConstants.cs
+++ server/DigitalReceipt/DigitalReceipt.Common/GlobalConstants.cs
@@ -1,4 +1,7 @@
-﻿namespace DigitalReceipt.Common
+﻿using System.Globalization;
+using System.Security.Authentication;
+
+namespace DigitalReceipt.Common
 {
     public static class GlobalConstants
     {
@@ -10,6 +13,12 @@
             public const int TokenLongExpirationTimeInDays = 7;
             public const int LockoutTimeInMinutes = 5;
             public const int MaxFailedAccessAttempts = 5;
+        }
+
+        public static class Roles
+        {
+            public const string Administrator = "Administrator";
+            public const string Cashier = "Cashier";
         }
     }
 }
---
+++ server/DigitalReceipt/DigitalReceipt.Data/Seeding/AdminSeeder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using DigitalReceipt.Data;
+using DigitalReceipt.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using static DigitalReceipt.Common.GlobalConstants;
+
+namespace DigitalReceipt.Data.Seeding
+{
+    public class AdminSeeder : ISeeder
+    {
+        public const string Username = "root";
+        public const string Password = "123456";
+
+        public async Task SeedAsync(ApplicationDbContext context, IServiceProvider serviceProvider)
+        {
+            UserManager<User> userManager = serviceProvider.GetRequiredService<UserManager<User>>();
+            User userFromDb = await userManager.FindByNameAsync(Username);
+
+            if (userFromDb != null)
+            {
+                return;
+            }
+
+            var user = new User
+            {
+                UserName = Username,
+                Email = "root@gmail.com",
+                EmailConfirmed = true
+            };
+
+            IdentityResult result = await userManager.CreateAsync(user, Password);
+            if (!result.Succeeded)
+            {
+                throw new Exception(string.Join(Environment.NewLine, result.Errors.Select(e => e.Description)));
+            }
+
+            await userManager.AddToRoleAsync(user, Roles.Administrator);
+        }
+    }
+}
